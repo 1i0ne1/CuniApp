27eac61c3afcda0f74e01750dcd05aa65578eb86
lionel sisso <sissolionel@gmail.com>
2026-07-08 11:15:38 +0100

    fix: remove mysqldump warning from SQL file header

--- db.sql
+++ db.sql
@@ -1,4 +1,3 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: cuniapp
@@ -1113,4 +1112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-07-08 11:12:32
+-- Dump completed on 2026-07-08 11:15:05
